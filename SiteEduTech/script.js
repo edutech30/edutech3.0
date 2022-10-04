3be4222b5bc29436efae35b42ce982a1c2575cc3
Edutech 3.0 <113441823+edutech30@users.noreply.github.com>
2022-10-03 22:09:49 -0300

    Delete script.js

--- SiteEduTech/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 200) {
-        nav.classList.add('active_nav');
-    } else {
-        nav.classList.remove('active_nav');
-    }
-});
